2cdd6cf7371f93f28673d1a7d0ad0c4a6018b324
ashvin-kfuse <ashvin.kumaran@kloudfuse.com>
2025-05-16 12:13:33 -0700

    improve delete pinot segment script (#45)

--- scripts/delete_bad_pinot_segments.sh
+++ scripts/delete_bad_pinot_segments.sh
@@ -3,9 +3,38 @@
 # create a dir for the table and run it from within that dir;
 # it will dump a bunch of status files in case things go wrong we can use to track
 # be careful on which controller you are connecting to
-
+set -x
 CONTROLLER=localhost:9000
-TABLE=kf_metrics_rollup_FIXME
+TABLE=""
+
+while :; do
+    case "$1" in
+        -t|--table)
+            TABLE="$2"
+            shift 2
+            ;;
+        -h|--help)
+            echo "Usage: $0 [-t|--table <table_name>] [-h|--help]"
+            exit 0
+            ;;
+        "") # No more arguments
+            break
+            ;;
+        -*)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+        *) # Unexpected extra argument
+            echo "Unexpected argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$TABLE" ]; then
+    echo "Error: Table name is required. Use -t or --table to specify the table name."
+    exit 1
+fi
 
 # Get a list of segments for a table
 curl -s "http://${CONTROLLER}/tables/${TABLE}_REALTIME/segmentsStatus" > "${TABLE}.new"
